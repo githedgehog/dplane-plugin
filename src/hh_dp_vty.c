86498f1ccf55de5b24f78762dbdd606f2fad0890
Fredi Raspall <fredi@githedgehog.com>
2025-01-14 15:12:20 +0100

    Add cmd to show version information of the plugin
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- src/hh_dp_vty.c
+++ src/hh_dp_vty.c
@@ -9,6 +9,22 @@
 #define HH_DP_RPC_STR "RPC stats\n"
 
 
+static void hh_vty_show_version(struct vty *vty) {
+    vty_out(vty, "\n Hedgehog-GW plugin %s\n", VER_STRING);
+    vty_out(vty, "  Built on %s (%s)\n", BUILD_DATE, BUILD_TYPE);
+    vty_out(vty, "   Commit: %s\n", GIT_COMMIT);
+    vty_out(vty, "   Branch: %s\n", GIT_BRANCH);
+    vty_out(vty, "   Tag   : %s\n\n", strlen(GIT_TAG) ? GIT_TAG : "none");
+}
+
+DEFUN (hh_dp_show_plugin_version, hh_dp_show_plugin_version_cmd,
+       "show hedgehog plugin version",
+       SHOW_STR HH_STR "plugin\n" "version\n")
+{
+    hh_vty_show_version(vty);
+    return CMD_SUCCESS;
+}
+
 DEFUN(hh_dp_show_rpc_stats, hh_dp_show_rpc_stats_cmd,
       "show hedgehog rpc stats",
       SHOW_STR HH_STR HH_DP_RPC_STR "show rpc stats\n")
@@ -21,5 +37,6 @@ DEFUN(hh_dp_show_rpc_stats, hh_dp_show_rpc_stats_cmd,
 void hh_dp_vty_init(void)
 {
     zlog_info("Initializing HHGW vty commands ...");
+    install_element(VIEW_NODE, &hh_dp_show_plugin_version_cmd);
     install_element(VIEW_NODE, &hh_dp_show_rpc_stats_cmd);
 }
